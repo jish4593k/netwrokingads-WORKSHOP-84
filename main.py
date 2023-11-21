77d4cf0bfee31e72061836bbd607b446270ff92e
jish4593k <143131599+jish4593k@users.noreply.github.com>
2023-11-22 00:03:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,87 +1,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix, accuracy_score
-from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.metrics import Accuracy
 
-def load_data(file_path):
-    # Load dataset
-    return pd.read_csv(file_path)
+# Load the dataset
+dataset = pd.read_csv('/Users/tharunpeddisetty/Desktop/Machine Learning A-Z (Codes and Datasets)/Part 3 - Classification/Section 14 - Logistic Regression/Python/Social_Network_Ads.csv')
 
-def preprocess_data(dataset):
-    # Extract features and target variable
-    X = dataset.iloc[:, :-1].values
-    Y = dataset.iloc[:, -1].values
-    
-    # Split the data into training and testing sets
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
+# Separate features and target variable
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
 
-    # Feature Scaling (Standardization)
-    sc = StandardScaler()
-    X_train = sc.fit_transform(X_train)
-    X_test = sc.transform(X_test)
+# Splitting data into training and testing sets
+np.random.seed(0)  # Set seed for reproducibility
+indices = np.random.permutation(len(X))
+split = int(0.75 * len(X))  # 75% training, 25% testing
 
-    return X_train, X_test, Y_train, Y_test
+X_train, X_test = X[indices[:split]], X[indices[split:]]
+Y_train, Y_test = Y[indices[:split]], Y[indices[split:]]
 
-def build_neural_network(input_dim):
-    # Build the neural network
-    model = Sequential()
-    model.add(Dense(units=6, activation='relu', input_dim=input_dim))
-    model.add(Dense(units=6, activation='relu'))
-    model.add(Dense(units=1, activation='sigmoid'))
-    
-    # Compile the model
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    
-    return model
+# Feature Scaling
+mean, std = X_train.mean(axis=0), X_train.std(axis=0)
+X_train = (X_train - mean) / std
+X_test = (X_test - mean) / std
 
-def train_neural_network(model, X_train, Y_train, epochs=100, batch_size=32):
-    # Train the model
-    model.fit(X_train, Y_train, epochs=epochs, batch_size=batch_size)
+# Build the neural network model
+model = Sequential([
+    Dense(units=6, activation='relu', input_dim=2),
+    Dense(units=1, activation='sigmoid')
+])
 
-def make_prediction(model, new_data, scaler):
-    # Make a prediction for new data
-    scaled_data = scaler.transform(np.array([new_data]))
-    return model.predict(scaled_data)[0][0]
+# Compile the model
+model.compile(optimizer=Adam(), loss=BinaryCrossentropy(), metrics=[Accuracy()])
 
-def evaluate_model(model, X_test, Y_test):
-    # Predicting for test data
-    y_pred = (model.predict(X_test) > 0.5).astype(int).flatten()
+# Train the model
+history = model.fit(X_train, Y_train, batch_size=32, epochs=50, verbose=1)
 
-    # Display predictions alongside actual values
-    print(np.concatenate((y_pred.reshape(len(y_pred), 1), Y_test.reshape(len(y_pred), 1)), 1))
+# Evaluate the model on the test set
+accuracy = model.evaluate(X_test, Y_test, verbose=0)[1]
+print("Test Accuracy:", accuracy)
 
-    # Confusion Matrix
-    cm = confusion_matrix(Y_test, y_pred)
-    print(cm)
-    
-    # Accuracy Score
-    print(f'Accuracy Score: {accuracy_score(Y_test, y_pred)}')
+# Plot the training history
+plt.plot(history.history['accuracy'])
+plt.title('Model Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.show()
 
-def main():
-    # Load and preprocess data
-    dataset = load_data('/Users/tharunpeddisetty/Desktop/Machine Learning A-Z (Codes and Datasets)/Part 3 - Classification/Section 14 - Logistic Regression/Python/Social_Network_Ads.csv')
-    X_train, X_test, Y_train, Y_test = preprocess_data(dataset)
-
-    # Build neural network
-    input_dim = X_train.shape[1]
-    model = build_neural_network(input_dim)
-
-    # Train neural network
-    train_neural_network(model, X_train, Y_train)
-
-    # Make prediction for new data
-    new_data = [30, 87000]
-    prediction = make_prediction(model, new_data, StandardScaler())
-
-    print(f'Prediction for new data: {prediction}')
-
-    # Evaluate the model
-    evaluate_model(model, X_test, Y_test)
-
-if __name__ == "__main__":
-    main()
+# Predicting for age=30, estimated salary=87000
+new_data = np.array([[30, 87000]])
+new_data_scaled = (new_data - mean) / std
+prediction = model.predict(new_data_scaled)
+print("Predicted probability:", prediction)
